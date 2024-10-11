d8f345f391afa6f256852a8456618ee22622f425
sossinayDev <125735344+sossinayDev@users.noreply.github.com>
2024-10-11 12:54:20 +0200

    Added analytics to EVERYTHING

--- analytics.js
+++ analytics.js
@@ -1,5 +1,6 @@
 console.log("Welcome to sossinaydev.github.io");
 const myDeviceId = '0362a647-9fdb-4671-a73c-efaeade4d212';
+const localDeviceId = 'd1cd73a3-0be1-4822-a13a-3a2955e276ce';
 
 let userId = localStorage.getItem('user_id');
 
@@ -14,24 +15,26 @@ currentFileName = currentFileName.replace(".html", "");
 console.log("Current file name:", currentFileName); // Log the current file name
 
 if (userId !== myDeviceId) {
-    const url = `https://sossinaydev-analyt-default-rtdb.firebaseio.com/homepage_sosDev/site-loads/${currentFileName}.json`;
+    if (userId !== localDeviceId) {
+        const url = `https://sossinaydev-analyt-default-rtdb.firebaseio.com/homepage_sosDev/site-loads/${currentFileName}.json`;
 
-    fetch(url)
-        .then(r => {
-            if (!r.ok) {
-                throw new Error(`HTTP error! status: ${r.status}`); // Capture non-200 responses
-            }
-            return r.json();
-        })
-        .then(d => {
-            return fetch(url, {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify((d || 0) + 1)
-            });
-        })
-        .then(() => console.log("Site visit logged."))
-        .catch(error => console.error("Error logging site visit:", error.message));
-} else {
-    console.log("Hello, sossinay");
+        fetch(url)
+            .then(r => {
+                if (!r.ok) {
+                    throw new Error(`HTTP error! status: ${r.status}`); // Capture non-200 responses
+                }
+                return r.json();
+            })
+            .then(d => {
+                return fetch(url, {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify((d || 0) + 1)
+                });
+            })
+            .then(() => console.log("Site visit logged."))
+            .catch(error => console.error("Error logging site visit:", error.message));
+    } else {
+        console.log("Hello, sossinay");
+    }
 }
